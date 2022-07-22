c03d5451386fe5583521a5a0d578d9be6dcb6e3c
n1l <92649708+n1l-arch@users.noreply.github.com>
2022-07-22 22:07:24 +0000

    Add files via upload

---
+++ Mouse settings.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+xinput --set-prop 'pointer:''Logitech G203 Prodigy Gaming Mouse' 'libinput Accel Profile Enabled' 0, 1
+xinput --set-prop 'pointer:''Logitech G203 Prodigy Gaming Mouse' 'libinput Accel Speed' 0 
---
+++ scrshot/fullscr.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+maim | xclip -selection clipboard -t image/png
